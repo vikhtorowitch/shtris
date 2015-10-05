5738de151b83dc62ce332d479a2dee5d68849601
vikhtorowitch <alikasoft@gmail.com>
2015-10-05 23:18:57 +0300

    to da se

--- Assets/Scripts/GridCubeEvents.cs
+++ Assets/Scripts/GridCubeEvents.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class GridCubeEvents : MonoBehaviour {
 
+	Transform cube;
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
-	
+		cube = GetComponent<Transform> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -14,7 +18,33 @@ public class GridCubeEvents : MonoBehaviour {
 	}
 
 	void OnMouseEnter () {
-		Debug.Log ("Mouse on");
+		//Debug.Log ("Mouse on");
 		//DestroyObject (this.gameObject);
+		//StartCoroutine (CubeRotateCoroutine (180f));
+
+	}
+
+
+	IEnumerator CubeRotateCoroutine(float _angle) {
+		float cr = cube.rotation.z;
+		if (cr < _angle) {
+			for(float i=cr;i<=_angle;i+=1) {
+				Vector3 vec = new Vector3();
+				vec.z = i;
+				cube.Rotate (vec);
+				Debug.Log(cube.rotation.z.ToString());
+				yield return new WaitForSeconds(0.001f);
+			}
+
+		} else {
+			for(float i=cr;i>=_angle;i-=1) {
+				Vector3 vec = new Vector3();
+				vec.z = i;
+				cube.Rotate (vec);
+				yield return new WaitForSeconds(0.001f);
+			}
+
+		}
+
 	}
 }
